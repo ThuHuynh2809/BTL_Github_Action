15fae2e23f32e187f66bad09deade3c76aa22e84
ThuHuynh2809 <huynhminhthu860@gmail.com>
2024-05-23 10:28:47 +0700

    update

--- src/main/java/com/example/githubactioncicd/GitHubActionCICDApplication.java
+++ src/main/java/com/example/githubactioncicd/GitHubActionCICDApplication.java
@@ -8,6 +8,8 @@ public class GitHubActionCICDApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitHubActionCICDApplication.class, args);
+
+        aaaa
     }
 
 }
